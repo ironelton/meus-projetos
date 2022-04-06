db7c91ffaf1c5094f2b46a80e712c7b0ba8846ad
ironelton <85509791+ironelton@users.noreply.github.com>
2022-04-05 21:51:04 -0300

    Update ex007.py

--- ex007.py
+++ ex007.py
@@ -1,5 +1,5 @@
-n1 = int(input('Digite a primeira nota:'))
-n2 = int(input('Digite a segunda  nota:'))
+n1 = float(input('Digite a primeira nota:'))
+n2 = float(input('Digite a segunda  nota:'))
 m = float((n1+n2)/2)
 print('A média das notas é:',m)
  
